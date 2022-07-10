c869cedd83045da12abb3b678da6d0e03186bda3
sam <contact@justsam.io>
2022-07-10 20:39:35 +0200

    chore: project bootstrap

---
+++ src/gear.rs
@@ -0,0 +1,11 @@
+
+
+pub struct Gear {
+    pycode: String
+}
+
+impl Gear {
+    pub fn run(py: Python) -> PyResult<()> {
+        
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+
+
+#[cfg(test)]
+mod tests{
+
+    #[test]
+    fn test_gear() {
+
+    }
+
+}
