b7d8fc4c7533c6378cae6d10489a0c7465f2b527
sam <contact@justsam.io>
2022-07-13 12:18:25 +0200

    chore(core): added communications module

---
+++ src/communications.rs
@@ -0,0 +1,119 @@
+use smallvec::SmallVec;
+use std::sync::Arc;
+
+use tract_core::prelude::*;
+
+pub enum Types {
+    PyModel,
+    MLModel,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub struct DGRequest<T> {
+    pub body: T,
+}
+
+impl<T> DGRequest<T> {
+    pub fn with_body(body: T) -> Self {
+        DGRequest { body }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+pub struct DGResponse<T> {
+    pub body: T,
+}
+
+impl<T> DGResponse<T> {
+    pub fn with_body(body: T) -> Self {
+        DGResponse { body }
+    }
+}
+
+pub(crate) trait DGRequestBase<T> {}
+pub(crate) trait DGResponseBase<T> {}
+
+impl<T> DGRequestBase<T> for T {}
+impl<T> DGResponseBase<T> for T {}
+
+pub struct PyModelRequest<K, V, T>
+where
+    K: hash::Hash + cmp::Eq + Default + ToPyObject,
+    V: Default + ToPyObject,
+    T: Default + ToPyObject,
+{
+    pub args: HashMap<K, V>,
+    pub kwargs: HashMap<&'static str, T>,
+}
+
+impl<K, V, T> PyModelRequest<K, V, T>
+where
+    K: hash::Hash + cmp::Eq + Default + ToPyObject,
+    V: Default + ToPyObject,
+    T: Default + ToPyObject,
+{
+    pub fn new() -> Self {
+        PyModelRequest {
+            ..Default::default()
+        }
+    }
+
+    pub fn with_args(mut self, args: HashMap<K, v>) -> Self {
+        self.args = args;
+        self
+    }
+
+    pub fn with_kwargs(mut self, kwargs: HashMap<&'static str, T>) -> Self {
+        self.kwargs = kwargs;
+        self
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct PyModelResponse {
+    response: PyDict,
+}
+
+impl PyModelResponse {
+    pub fn new() -> Self {
+        PyModelResponse {
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct MLModelRequest {
+    pub input: Tensor,
+}
+
+impl MLModelRequest {
+    pub fn new() -> Self {
+        MLModelRequest {
+            ..Default::default()
+        }
+    }
+
+    pub fn body(mut self, input: Tensor) -> Self {
+        self.input = input;
+        self
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct MLModelResponse {
+    pub output: SmallVec<[Arc<Tensor>; 4]>,
+}
+
+impl MLModelResponse {
+    pub fn new() -> Self {
+        MLModelResponse {
+            ..Default::default()
+        }
+    }
+
+    pub fn with_output(mut self, output: SmallVec<[Arc<Tensor>; 4]>) -> Self {
+        self.output = output;
+        self
+    }
+}
