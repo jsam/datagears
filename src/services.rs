91c22974def1fb33b96119f1ebb50452b2946cc5
sam <contact@justsam.io>
2022-07-13 12:47:21 +0200

    chore(services): added service definitions

--- src/services.rs
+++ src/services.rs
@@ -1,1 +1,34 @@
-use crate::errors::*;
+use std::{cmp, hash};
+
+use crate::{
+    communications::{DGRequest, DGResponse, MLModelRequest, MLModelResponse, PyModelRequest},
+    errors::*,
+};
+use async_trait::async_trait;
+use pyo3::ToPyObject;
+
+pub(crate) trait Service {
+    fn load(&mut self) -> Result<()>;
+}
+
+#[async_trait]
+pub(crate) trait PyModelService {
+    type FutType;
+
+    async fn async_process<K: 'static, V: 'static, T: 'static>(
+        &self,
+        request: DGRequest<PyModelRequest<K, V, T>>,
+    ) -> Self::FutType
+    where
+        K: hash::Hash + cmp::Eq + Default + ToPyObject + Send,
+        V: Default + ToPyObject + Send,
+        T: Default + ToPyObject + Send;
+}
+
+#[async_trait]
+pub(crate) trait MLModelService {
+    async fn async_process(
+        &self,
+        request: DGRequest<MLModelRequest>,
+    ) -> Result<DGResponse<MLModelResponse>>;
+}
