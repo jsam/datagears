b1b8e04903ebe67db751b4594b5a5b9d5252489e
sam <contact@justsam.io>
2022-07-15 16:56:18 +0200

    chore: code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,3 @@ pub mod errors;
 pub mod gears;
 pub mod macros;
 pub mod services;
-
-// struct _interpreter();
-
-// impl _interpreter {
-//     fn new() -> Self {
-//         pyo3::prepare_freethreaded_python();
-//         // append_to_inittab!("requests");
-//         // Python::with_gil(|py| {
-
-//         //     let pip_internal = py.import("pip._internal")?;
-//         //     let main: String = pip_internal.getattr("main")?.extract()?;
-
-//         //     let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-//         //     let user: String = py.eval(code, None, Some(&locals))?.extract()?;
-
-//         //     println!("Hello {}, I'm Python {}", user, version);
-//         //     Ok(())
-//         // });
-
-//         Self ()
-//     }
-// }
-
-// //static mut DATAGEARS_PYTHON: _interpreter = _interpreter::new();
